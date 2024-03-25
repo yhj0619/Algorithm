de4967bb8fb7a4fe80a258d0bd7371dbafbd7057
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2024-03-25 18:27:44 +0900

    [Bronze V] Title: A-B, Time: 136 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int num1 = Integer.parseInt(st.nextToken());
+		int num2 = Integer.parseInt(st.nextToken());
+		
+		System.out.println(num1 - num2);
+		
+	}
+
+}

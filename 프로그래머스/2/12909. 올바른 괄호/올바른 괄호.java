92ce4386a934a1d7e8cfcb59f2131d9a4dc8c6a6
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2025-01-08 21:28:33 +0900

    [level 2] Title: 올바른 괄호, Time: 16.71 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(c == '(')
+                stack.push(c);
+            else if(c == ')'){
+                if(stack.size() == 0)
+                    return false;
+                else
+                    stack.pop();
+            }
+        }
+        
+        if(stack.size() != 0)
+            answer = false;
+
+        return answer;
+    }
+}

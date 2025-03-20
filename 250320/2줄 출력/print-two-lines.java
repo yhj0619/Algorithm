5b6e5dd69d42d069b3e77e1b2c3fb50e69401a32
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2025-03-20 19:08:05 +0900

    Title: 2줄 출력, Time: 80ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250320/2줄 출력/print-two-lines.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        System.out.println("Hello" + "\nWorld");
+    }
+}

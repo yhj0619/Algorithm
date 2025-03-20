011610fe6a264736c36f812b8b911cd2175648e5
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2025-03-20 19:21:13 +0900

    Title: 사칙연산 연습, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250320/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int a = 5;
+        int b = 30;
+
+        int c = a+b;
+
+        System.out.println(c/8 + "\n" + c%8);
+
+    }
+}

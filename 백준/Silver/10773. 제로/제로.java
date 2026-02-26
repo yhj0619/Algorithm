c8b71dca54db658bd9fb09b01f093f50f6b69cd8
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2026-02-26 22:10:54 +0900

    [Silver IV] Title: 제로, Time: 204 ms, Memory: 23564 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(num != 0){
+                stack.push(num);
+            } else{
+                stack.pop();
+            }
+        }
+        int sum = 0;
+        for(int num : stack){
+            sum+=num;
+        }
+        System.out.println(sum);
+    }
+}

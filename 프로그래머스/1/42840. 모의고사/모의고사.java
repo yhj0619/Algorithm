c5c5f11ca15844f9d0679ec0932401150afab348
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2026-03-06 21:26:09 +0900

    [level 1] Title: 모의고사, Time: 0.93 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,34 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
+
+        int[] one = {1,2,3,4,5};
+        int[] two = {2,1,2,3,2,4,2,5};
+        int[] three = {3,3,1,1,2,2,4,4,5,5};
         
-        int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] scores = new int[3];
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == one[i % one.length]) scores[0]++;
+            if(answers[i] == two[i % two.length]) scores[1]++;
+            if(answers[i] == three[i % three.length]) scores[2]++;
+        }
+                
+        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
         
-        int a = 0; 
-        int b = 0; 
-        int c = 0;
+        ArrayList<Integer> list = new ArrayList<>();
         
-        for(int i=0; i < answers.length; i++){
-           if(answers[i] == one[i % one.length]) a++;
-           if(answers[i] == two[i % two.length]) b++;
-           if(answers[i] == three[i % three.length]) c++;       
+        for(int i = 0; i < 3; i++){
+            if(scores[i] == max){
+                list.add(i+1);
+            }
         }
         
-        int max= Math.max(Math.max(a, b), c);
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        
-        if(max == a)
-            result.add(1);
-        if(max == b)
-            result.add(2);
-        if(max == c)
-            result.add(3);
-        
-        int[] answer = new int[result.size()];
-        for(int i=0; i<result.size(); i++){
-            answer[i] = result.get(i);            
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
         }
         
         return answer;

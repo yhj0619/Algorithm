311dccb988aa973b1509ad37fad5f09de0bd4d7a
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2026-03-07 12:13:47 +0900

    [level 2] Title: 피로도, Time: 3.05 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,27 @@
+import java.util.*;
+
 class Solution {
-    int answer = 0;
-    boolean[] visited;
+    
+    static boolean[] visited;
+    static int answer = -1;
+    
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
+        visited = new boolean[dungeons.length + 1];
         
         dfs(k, dungeons, 0);
         
         return answer;
     }
     
-    public void dfs(int k, int[][] dungeons, int depth) {
-        for(int i=0; i < dungeons.length; i++){
+    static void dfs(int k, int[][] dungeons, int depth){
+        answer = Math.max(answer, depth);
+        
+        for(int i = 0; i < dungeons.length; i++){
             if(!visited[i] && k >= dungeons[i][0]){
                 visited[i] = true;
-                dfs(k-dungeons[i][1], dungeons, depth+1);
+                dfs(k - dungeons[i][1], dungeons, depth+1);
                 visited[i] = false;
             }
         }
-        answer = Math.max(answer, depth);
     }
 }

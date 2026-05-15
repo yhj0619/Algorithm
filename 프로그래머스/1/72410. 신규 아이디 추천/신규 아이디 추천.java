5c6ed6504afda387b354a50343e95dda79d9d4d9
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2026-05-15 19:49:49 +0900

    [level 1] Title: 신규 아이디 추천, Time: 13.42 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String solution(String new_id) {
+        // 1단계
+        new_id = new_id.toLowerCase();
+
+        // 2단계
+        new_id = new_id.replaceAll("[^a-z0-9-_.]", "");
+
+        // 3단계
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+
+        // 4단계
+        new_id = new_id.replaceAll("^\\.|\\.$", "");
+
+        // 5단계
+        if (new_id.isEmpty()) {
+            new_id = "a";
+        }
+
+        // 6단계
+        if (new_id.length() >= 16) {
+            new_id = new_id.substring(0, 15);
+            new_id = new_id.replaceAll("\\.$", "");
+        }
+
+        // 7단계
+        while (new_id.length() <= 2) {
+            new_id += new_id.charAt(new_id.length() - 1);
+        }
+
+        return new_id;
+    }
+}
